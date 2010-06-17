18761dc4d49521160def7de89c3c5aced2a7fb60
mrs15 <mrs15@69157e9a-d921-0410-9500-c7e1546d874a>
2010-06-17 00:09:56 +0000

    added frequently fragmented tables to weekly db optimization script
    git-svn-id: http://iss-tech.humboldt.edu/repos/learn/branches/moodle198@1267 69157e9a-d921-0410-9500-c7e1546d874a

--- admin/custom/db_optimizations.php
+++ admin/custom/db_optimizations.php
@@ -8,6 +8,9 @@ $sqls = array();
 $sqls[] = 'OPTIMIZE TABLE '.$CFG->prefix.'sessions;';
 $sqls[] = 'OPTIMIZE TABLE '.$CFG->prefix.'cache_text;';
 $sqls[] = 'OPTIMIZE TABLE '.$CFG->prefix.'cache_filters;';
+$sqls[] = 'OPTIMIZE TABLE '.$CFG->prefix.'course_sections;';
+$sqls[] = 'OPTIMIZE TABLE '.$CFG->prefix.'forum_posts;';
+$sqls[] = 'OPTIMIZE TABLE '.$CFG->prefix.'log;';
 
 foreach($sqls as $sql) {
     if (execute_sql($sql, false)) {
